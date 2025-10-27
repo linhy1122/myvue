eb5c6524c993ede3c24124123ec6181d81cf11c1
linhy1122 <1768013886@qq.com>
2025-10-27 21:43:27 +0800

    a page test

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.jpg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="Ciallo!!!!!" />
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Ciallo</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/ciallo">Ciallo</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/CialloView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ciallo">
+    <img alt="Vue logo" class="logo" src="@/assets/C.webp" width="125" height="125" />
+    <h1>CIALLO!!!!!! </h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .ciallo {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
